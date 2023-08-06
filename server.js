eefc5036f41f02285d6df00b7aebc653f38ced94
jbtokam <85589835+jeanbTokam@users.noreply.github.com>
2023-08-06 02:55:35 -0400

    Deployed

--- server.js
+++ server.js
@@ -12,18 +12,8 @@ app.use('/api/users/',userRoute)
 app.use('/api/orders/',orderRoute)
 
 
-    if(process.env.NODE_ENV === 'production')
-    {
-        app.use('/' , express.static('client/build'))
-
-        app.get('*' , (req,res)=>{
-
-            res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-
-        })
-
-
-    }
+ 
+app.use(express.static(path.join(__dirname + "/public")));
 
    
 
